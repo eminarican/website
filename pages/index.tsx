cd24b1fb5201c50973877662a92c76d2608eeea7
eminarican <emin.arican@outlook.com>
2021-08-01 17:23:42 +0300

    some basic card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 
+const cardStyle: React.CSSProperties = {
+  backdropFilter: "blur(20px)"
+}
+
 export default class extends React.Component {
 
   render() {
-    return (<div></div>)
+    return (<div className="h-screen w-screen px-4 py-4">
+      <div style={cardStyle} className="h-full w-full bg-black bg-opacity-50 rounded-xl"></div>
+    </div>)
   }
 }
