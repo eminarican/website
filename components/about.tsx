7262550c0c8ce47bb7ea747f8f7ca34d679783bc
Emin Arıcan <emin.arican@outlook.com>
2021-08-12 18:11:30 +0300

    select none

--- components/about.tsx
+++ components/about.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default class About extends React.Component {
     render() {
-        return (<div className="h-full w-full flex justify-center items-center">
+        return (<div className="h-full w-full flex justify-center items-center select-none">
             <img
                 src="https://avatars.githubusercontent.com/u/17065574?v=4"
                 className="rounded-xl"
