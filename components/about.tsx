0c1db7ff768c9078bf79bd2b1052a1cac447b411
Emin Arıcan <emin.arican@outlook.com>
2021-08-12 18:04:27 +0300

    revert img

--- components/about.tsx
+++ components/about.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Image from "next/image";
 
 export default class About extends React.Component {
     render() {
         return (<div className="h-full w-full flex justify-center items-center">
-            <Image
+            <img
                 src="https://avatars.githubusercontent.com/u/17065574?v=4"
                 className="rounded-xl"
                 height="200px"
