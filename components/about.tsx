6d1a68408b2d57a6a3cc050af0af6229e6ac5236
Emin Arıcan <emin.arican@outlook.com>
2021-08-12 18:27:31 +0300

    image draggable false

--- components/about.tsx
+++ components/about.tsx
@@ -5,7 +5,8 @@ export default class About extends React.Component {
         return (<div className="h-full w-full flex justify-center items-center select-none">
             <img
                 src="https://avatars.githubusercontent.com/u/17065574?v=4"
-                className="rounded-xl select-none"
+                className="rounded-xl"
+                draggable="false"
                 height="200px"
                 width="200px"
                 alt="pfp"
