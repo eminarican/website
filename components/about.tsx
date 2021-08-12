95a4e67127a417787e63249625761ce96369c34d
Emin Arıcan <emin.arican@outlook.com>
2021-08-12 18:24:29 +0300

    image select none

--- components/about.tsx
+++ components/about.tsx
@@ -5,7 +5,7 @@ export default class About extends React.Component {
         return (<div className="h-full w-full flex justify-center items-center select-none">
             <img
                 src="https://avatars.githubusercontent.com/u/17065574?v=4"
-                className="rounded-xl"
+                className="rounded-xl select-none"
                 height="200px"
                 width="200px"
                 alt="pfp"
