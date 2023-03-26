077a6ea2c18765a98c9ab4f4fb527063a1ab8ad1
Emin Arıcan <emin.arican@outlook.com>
2023-03-26 06:42:30 +0300

    update blog navbar

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,8 +1,8 @@
 <Frame header={false}>
     <div class="absolute flex flex-row px-5 py-4 gap-x-5 font-mono text-sm text-white">
-        <p class="underline underline-offset-2 cursor-pointer" on:click={() => exit()}>{"< exit"}</p>
-        <p class="text-gray-400 cursor-default">home</p>
-        <p class="text-gray-400 cursor-default">blogs</p>
+        <button class="underline underline-offset-2 cursor-pointer" on:click={() => exit()}>{"< exit"}</button>
+        <button class="text-gray-400 cursor-default">home</button>
+        <button class="text-gray-400 cursor-default">blogs</button>
     </div>
     <slot></slot>
 </Frame>
