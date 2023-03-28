6a8b06e209e9c2bbb0cdc8672a95c5ccc840e29e
Emin Arıcan <emin.arican@outlook.com>
2023-03-29 00:09:21 +0300

    no need for extra func

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,6 +1,6 @@
 <Frame header={false}>
     <div class="absolute flex flex-row px-5 py-4 gap-x-5 font-mono text-sm text-white">
-        <button class="underline underline-offset-2 cursor-pointer" on:click={() => exit()}>{"< exit"}</button>
+        <button class="underline underline-offset-2 cursor-pointer" on:click={() => goto("/")}>{"< exit"}</button>
         <button class="text-gray-400 cursor-default">home</button>
         <button class="text-gray-400 cursor-default">blogs</button>
     </div>
@@ -11,8 +11,4 @@
     import {goto} from "$app/navigation";
 
     import Frame from "../../parts/Frame.svelte";
-
-    function exit() {
-        goto("/");
-    }
 </script>
