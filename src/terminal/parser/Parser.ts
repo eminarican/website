5fb2408b3e7ca7e831eb9d973fe96a9faba88acc
Emin Arıcan <emin.arican@outlook.com>
2023-03-31 17:47:12 +0300

    update parser

--- src/terminal/parser/Parser.ts
+++ src/terminal/parser/Parser.ts
@@ -5,6 +5,14 @@ import {CommandAction, PipeAction, ThenAction} from "./Action";
 
 export default class Parser {
 
+    public actions: Array<Action>;
+    public current: CommandAction | null;
+
+    public constructor() {
+        this.actions = [];
+        this.current = null;
+    }
+
     public static parse(input: string): Array<Action> {
         let tokens = input.match(/("[^"]+"|\S+)/g)?.map((parameter) => {
             switch (parameter) {
@@ -21,43 +29,56 @@ export default class Parser {
             return new Token(TokenType.Arg, parameter);
         });
 
-        let actions: Array<Action> = [];
-        let current: CommandAction | null = null;
+        let parser = new Parser();
 
         tokens?.forEach((token) => {
-            let data = token.data;
+            if (parser.parseCommand(token)) return;
 
-            switch (token.type) {
-                case TokenType.Arg:
-                    if (current == null) {
-                        current = new CommandAction(data);
-                    } else {
-                        current.args.push(data);
-                    }
-                    return;
-                case TokenType.Flag:
-                    current?.flags.push(data);
-                    return;
-            }
-
-            if (current != null) {
-                actions.push(current);
-                current = null;
-            }
-
-            switch (token.type) {
-                case TokenType.Pipe:
-                    actions.push(new PipeAction());
-                    break;
-                case TokenType.Then:
-                    actions.push(new ThenAction());
-            }
+            parser.append();
+            parser.parseOperator(token);
         });
 
-        if (current != null) {
-            actions.push(current);
-        }
+        parser.finish();
+        console.log(parser.actions)
+        return parser.actions;
+    }
 
-        return actions;
+    public parseCommand(token: Token): boolean {
+        switch (token.type) {
+            case TokenType.Arg:
+                if (this.current == null) {
+                    this.current = new CommandAction(token.data);
+                } else {
+                    this.current.args.push(token.data);
+                }
+                return true;
+            case TokenType.Flag:
+                this.current?.flags.push(token.data);
+                return true;
+        }
+        return false;
+    }
+
+    public parseOperator(token: Token) {
+        switch (token.type) {
+            case TokenType.Pipe:
+                this.actions.push(new PipeAction());
+                break;
+            case TokenType.Then:
+                this.actions.push(new ThenAction());
+                break;
+        }
+    }
+
+    public append() {
+        if (this.current == null) return;
+        this.actions.push(this.current);
+        this.current = null;
+    }
+
+    public finish() {
+        if (this.current != null) {
+            this.actions.push(this.current);
+        }
     }
 }
