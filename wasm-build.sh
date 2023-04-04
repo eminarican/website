ef97c0464aa9e64f399202c9bb6299a677e445d8
Emin Arıcan <emin.arican@outlook.com>
2023-04-04 22:38:19 +0300

    move

--- wasm-build.sh
+++ wasm-build.sh
@@ -2,3 +2,4 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 cargo install rsw
+rsw build
