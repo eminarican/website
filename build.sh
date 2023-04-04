072c518df17bda2a6171a094895bbaaf881a5ac7
Emin Arıcan <emin.arican@outlook.com>
2023-04-04 22:20:12 +0300

    oopsie

--- build.sh
+++ build.sh
@@ -2,4 +2,3 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 cargo install rsw
-npm run build
