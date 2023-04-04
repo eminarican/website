786c80cd3e60f740a3345b3e2c5805ccfd6c415d
Emin Arıcan <emin.arican@outlook.com>
2023-04-04 22:44:19 +0300

    nightly

--- wasm-build.sh
+++ wasm-build.sh
@@ -1,4 +1,4 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile default --default-toolchain nightly
 source "$HOME/.cargo/env"
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 cargo install rsw
